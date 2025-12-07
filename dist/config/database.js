015b683fc5df83daedc5d788f2cb587ee18385c2
Elodie Decraie <elomarie17@gmail.com>
2025-12-07 22:59:38 +0100

    Delete dist directory

--- dist/config/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize({
-    dialect: "sqlite",
-    storage: "./database.sqlite", // le fichier SQLite sera créé ici
-    logging: false, // désactive les logs SQL
-});
-exports.default = sequelize;
